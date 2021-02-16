01c6e9302393bb68915d85f4ede75fed497f77b0
ashwinashok98 <ashwinashok98@gmail.com>
2021-02-16 14:22:28 +0530

    made basic layout and folder structure

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxi";
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build controller</div> 
+      </Aux>
+    );
+  }
+}
+export default BurgerBuilder;
